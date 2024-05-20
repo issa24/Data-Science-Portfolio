ec28122ec18338cca9a7e860258fe61cc588639b
Issa <figos21@gmail.com>
2024-05-19 19:39:29 -0700

    Updated resume links

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,18 +1,58 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  padding: 2rem;
+  max-width: 800px;
+  margin: auto;
+  text-align: center;
+`;
+
+const Title = styled.h1`
+  font-size: 2rem;
+  color: #333;
+  margin-bottom: 1rem;
+`;
+
+const Description = styled.p`
+  font-size: 1.2rem;
+  color: #666;
+  margin-bottom: 2rem;
+`;
+
+const Button = styled.a`
+  display: inline-block;
+  padding: 0.7rem 1.5rem;
+  font-size: 1rem;
+  color: white;
+  background-color: #61dafb;
+  border: none;
+  border-radius: 4px;
+  text-decoration: none;
+  margin: 0.5rem;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #21a1f1;
+  }
+`;
 
 const Resume = () => (
-  <div className="container-small">
+  <Container>
     <Helmet>
       <title>Resume - Data Science Portfolio</title>
     </Helmet>
-    <h1 className="title">My Resume</h1>
-    <p className="description">
-      You can view or download my resume using the links below.
-    </p>
-    <a href={`${process.env.PUBLIC_URL}/pdf/resume.pdf`} target="_blank" className="button">View Resume</a>
-    <a href={`${process.env.PUBLIC_URL}/pdf/resume.pdf`} download className="button">Download Resume</a>
-  </div>
+    <Title>My Resume</Title>
+    <Description>You can view or download my resume using the links below.</Description>
+    <Button href={`${process.env.PUBLIC_URL}/pdf/resume.pdf`} target="_blank" rel="noopener noreferrer">
+      View Resume
+    </Button>
+    <Button href={`${process.env.PUBLIC_URL}/pdf/resume.pdf`} download>
+      Download Resume
+    </Button>
+  </Container>
 );
 
 export default Resume;
