87a1573cbc94f9e0fe95eb0cfde1c34fbeca98df
Issa <figos21@gmail.com>
2024-05-19 20:52:33 -0700

    Updated button spacing on mobile devies

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,7 +21,7 @@ const HeroSection = styled.div`
   max-width: 1200px;
   margin: auto;
   padding: 2rem;
-  background: white;
+  background: adb5bd ;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   border-radius: 8px;
 `;
